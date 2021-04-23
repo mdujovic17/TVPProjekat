356dd68c0348e59f3096122fea496258955c78d0
Marko Dujović <dujovicm@gmail.com>
2021-04-23 22:57:09 +0200

    Rezervacija prototip

--- TVPProjekat/TVPProjekat/forms/pomocne/FormRezervacija.Designer.cs
+++ TVPProjekat/TVPProjekat/forms/pomocne/FormRezervacija.Designer.cs
@@ -30,12 +30,17 @@ namespace TVPProjekat.forms.pomocne
         private void InitializeComponent()
         {
             this.lvProjekcije = new System.Windows.Forms.ListView();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clmImeFilma = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmTrajanje = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmCenaKarte = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDatumIVreme = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmSala = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmBrojDostupnihSedista = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.numBrojMesta = new System.Windows.Forms.NumericUpDown();
+            this.txtCena = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnRezervisi = new System.Windows.Forms.Button();
             this.datePocetniDatum = new System.Windows.Forms.DateTimePicker();
             this.dateKrajniDatum = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
@@ -44,13 +49,9 @@ namespace TVPProjekat.forms.pomocne
             this.label6 = new System.Windows.Forms.Label();
             this.comboNazivFilma = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.clmImeFilma = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clmTrajanje = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clmCenaKarte = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clmDatumIVreme = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clmSala = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clmBrojDostupnihSedista = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numBrojMesta)).BeginInit();
             this.SuspendLayout();
             // 
             // lvProjekcije
@@ -71,122 +72,6 @@ namespace TVPProjekat.forms.pomocne
             this.lvProjekcije.UseCompatibleStateImageBehavior = false;
             this.lvProjekcije.View = System.Windows.Forms.View.Details;
             this.lvProjekcije.SelectedIndexChanged += new System.EventHandler(this.selektujObjekat);
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(74, 287);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(278, 287);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(200, 289);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Ukupna cena";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 289);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Broj mesta";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(384, 285);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(310, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Rezerviši";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 10);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Rezervacija projekcije";
-            // 
-            // datePocetniDatum
-            // 
-            this.datePocetniDatum.Location = new System.Drawing.Point(12, 63);
-            this.datePocetniDatum.Name = "datePocetniDatum";
-            this.datePocetniDatum.Size = new System.Drawing.Size(174, 20);
-            this.datePocetniDatum.TabIndex = 7;
-            // 
-            // dateKrajniDatum
-            // 
-            this.dateKrajniDatum.Location = new System.Drawing.Point(192, 63);
-            this.dateKrajniDatum.Name = "dateKrajniDatum";
-            this.dateKrajniDatum.Size = new System.Drawing.Size(174, 20);
-            this.dateKrajniDatum.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 47);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(75, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Početni datum";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(189, 47);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Krajnji datum";
-            // 
-            // comboSale
-            // 
-            this.comboSale.FormattingEnabled = true;
-            this.comboSale.Location = new System.Drawing.Point(372, 62);
-            this.comboSale.Name = "comboSale";
-            this.comboSale.Size = new System.Drawing.Size(121, 21);
-            this.comboSale.TabIndex = 11;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(369, 47);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(28, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Sala";
-            // 
-            // comboNazivFilma
-            // 
-            this.comboNazivFilma.FormattingEnabled = true;
-            this.comboNazivFilma.Location = new System.Drawing.Point(500, 62);
-            this.comboNazivFilma.Name = "comboNazivFilma";
-            this.comboNazivFilma.Size = new System.Drawing.Size(194, 21);
-            this.comboNazivFilma.TabIndex = 13;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(499, 47);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(58, 13);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Naziv filma";
             // 
             // clmImeFilma
             // 
@@ -218,11 +103,148 @@ namespace TVPProjekat.forms.pomocne
             this.clmBrojDostupnihSedista.Text = "Broj dostupnih mesta";
             this.clmBrojDostupnihSedista.Width = 130;
             // 
+            // numBrojMesta
+            // 
+            this.numBrojMesta.Location = new System.Drawing.Point(74, 287);
+            this.numBrojMesta.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numBrojMesta.Name = "numBrojMesta";
+            this.numBrojMesta.Size = new System.Drawing.Size(120, 20);
+            this.numBrojMesta.TabIndex = 1;
+            this.numBrojMesta.ValueChanged += new System.EventHandler(this.racunajCenu);
+            // 
+            // txtCena
+            // 
+            this.txtCena.Location = new System.Drawing.Point(278, 287);
+            this.txtCena.Name = "txtCena";
+            this.txtCena.ReadOnly = true;
+            this.txtCena.Size = new System.Drawing.Size(100, 20);
+            this.txtCena.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ukupna cena";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Broj mesta";
+            // 
+            // btnRezervisi
+            // 
+            this.btnRezervisi.Location = new System.Drawing.Point(384, 285);
+            this.btnRezervisi.Name = "btnRezervisi";
+            this.btnRezervisi.Size = new System.Drawing.Size(310, 23);
+            this.btnRezervisi.TabIndex = 5;
+            this.btnRezervisi.Text = "Rezerviši";
+            this.btnRezervisi.UseVisualStyleBackColor = true;
+            this.btnRezervisi.Click += new System.EventHandler(this.rezervisi);
+            // 
+            // datePocetniDatum
+            // 
+            this.datePocetniDatum.Location = new System.Drawing.Point(12, 25);
+            this.datePocetniDatum.Name = "datePocetniDatum";
+            this.datePocetniDatum.Size = new System.Drawing.Size(174, 20);
+            this.datePocetniDatum.TabIndex = 7;
+            // 
+            // dateKrajniDatum
+            // 
+            this.dateKrajniDatum.Location = new System.Drawing.Point(192, 25);
+            this.dateKrajniDatum.Name = "dateKrajniDatum";
+            this.dateKrajniDatum.Size = new System.Drawing.Size(174, 20);
+            this.dateKrajniDatum.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Početni datum";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Krajnji datum";
+            // 
+            // comboSale
+            // 
+            this.comboSale.FormattingEnabled = true;
+            this.comboSale.Location = new System.Drawing.Point(372, 24);
+            this.comboSale.Name = "comboSale";
+            this.comboSale.Size = new System.Drawing.Size(121, 21);
+            this.comboSale.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(369, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Sala";
+            // 
+            // comboNazivFilma
+            // 
+            this.comboNazivFilma.FormattingEnabled = true;
+            this.comboNazivFilma.Location = new System.Drawing.Point(500, 24);
+            this.comboNazivFilma.Name = "comboNazivFilma";
+            this.comboNazivFilma.Size = new System.Drawing.Size(194, 21);
+            this.comboNazivFilma.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(499, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Naziv filma";
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(12, 52);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(545, 31);
+            this.btnFilter.TabIndex = 15;
+            this.btnFilter.Text = "Filtriraj prikaz";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.filtriraj);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(564, 52);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(130, 31);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.Text = "Resetuj filter";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.resetFilter);
+            // 
             // FormRezervacija
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(706, 318);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnFilter);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.comboNazivFilma);
             this.Controls.Add(this.label6);
@@ -231,18 +253,17 @@ namespace TVPProjekat.forms.pomocne
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateKrajniDatum);
             this.Controls.Add(this.datePocetniDatum);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRezervisi);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtCena);
+            this.Controls.Add(this.numBrojMesta);
             this.Controls.Add(this.lvProjekcije);
             this.Name = "FormRezervacija";
             this.Text = "FormRezervacija";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.zatvori);
             this.Load += new System.EventHandler(this.ucitajPodatke);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBrojMesta)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,12 +272,11 @@ namespace TVPProjekat.forms.pomocne
         #endregion
 
         private System.Windows.Forms.ListView lvProjekcije;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numBrojMesta;
+        private System.Windows.Forms.TextBox txtCena;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRezervisi;
         private System.Windows.Forms.DateTimePicker datePocetniDatum;
         private System.Windows.Forms.DateTimePicker dateKrajniDatum;
         private System.Windows.Forms.Label label4;
@@ -271,5 +291,7 @@ namespace TVPProjekat.forms.pomocne
         private System.Windows.Forms.ColumnHeader clmDatumIVreme;
         private System.Windows.Forms.ColumnHeader clmSala;
         private System.Windows.Forms.ColumnHeader clmBrojDostupnihSedista;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnReset;
     }
 }
--- TVPProjekat/TVPProjekat/forms/pomocne/FormRezervacija.cs
+++ TVPProjekat/TVPProjekat/forms/pomocne/FormRezervacija.cs
@@ -16,6 +16,8 @@ namespace TVPProjekat.forms.pomocne
     {
         private static Form frmKupac;
 
+        double cena = 0.00;
+
         List<Sala> sale;
         List<Film> filmovi;
         List<Projekcija> projekcije;
@@ -28,13 +30,23 @@ namespace TVPProjekat.forms.pomocne
             sale = new List<Sala>();
             filmovi = new List<Film>();
             projekcije = new List<Projekcija>();
+            btnRezervisi.Enabled = false;
+        }
+
+        public void listUpdate()
+        {
+            sale = new List<Sala>();
+            filmovi = new List<Film>();
+            projekcije = new List<Projekcija>();
+
+            LocalFileManager.JSONDeserialize(sale, "sale");
+            LocalFileManager.JSONDeserialize(filmovi, "filmovi");
+            LocalFileManager.JSONDeserialize(projekcije, "projekcije");
         }
 
         private void ucitajPodatke(object sender, EventArgs e)
         {
-            LocalFileManager.JSONDeserialize(sale, "sale");
-            LocalFileManager.JSONDeserialize(filmovi, "filmovi");
-            LocalFileManager.JSONDeserialize(projekcije, "projekcije");
+            listUpdate();
 
             foreach (Sala sala in sale)
             {
@@ -49,6 +61,7 @@ namespace TVPProjekat.forms.pomocne
                 ListViewItem item = new ListViewItem(new[] { projekcija.Film.ImeFilma, projekcija.Film.Trajanje.ToString() + " min", projekcija.CenaKarte.ToString("0.00") + " RSD", projekcija.DatumProjekcije.ToString("dd/MM/yyyy") + " " + projekcija.VremeProjekcije.ToString("HH:mm"), "Sala " + projekcija.Sala.ToString(), projekcija.DostupnaMesta.ToString() });
                 lvProjekcije.Items.Add(item);
             }
+            txtCena.Text = cena.ToString("0.00");
         }
 
         private void selektujObjekat(object sender, EventArgs e)
@@ -68,6 +81,8 @@ namespace TVPProjekat.forms.pomocne
                     break;
                 }
             }
+
+            racunajCenu(sender, e);
         }
 
         public static void primiFormu(FormKupac forma)
@@ -80,6 +95,68 @@ namespace TVPProjekat.forms.pomocne
             this.Dispose();
             this.Close();
             frmKupac.Show();
+        }
+
+        private void filtriraj(object sender, EventArgs e)
+        {
+            listViewClear();
+            foreach (Projekcija projekcija in projekcije)
+            {
+                ListViewItem item = new ListViewItem(new[] { projekcija.Film.ImeFilma, projekcija.Film.Trajanje.ToString() + " min", projekcija.CenaKarte.ToString("0.00") + " RSD", projekcija.DatumProjekcije.ToString("dd/MM/yyyy") + " " + projekcija.VremeProjekcije.ToString("HH:mm"), "Sala " + projekcija.Sala.ToString(), projekcija.DostupnaMesta.ToString() });
+                
+                bool datum = datePocetniDatum.Value < projekcija.DatumProjekcije && dateKrajniDatum.Value > projekcija.DatumProjekcije;
+                bool sala = comboSale.SelectedIndex != -1 && projekcija.Sala == int.Parse(comboSale.SelectedItem.ToString().Replace("Sala ", ""));
+                bool imeFilma = comboNazivFilma.SelectedIndex != -1 && projekcija.Film.ImeFilma.Equals(comboNazivFilma.SelectedItem.ToString());
+
+                if (datum)
+                {
+                    if (sala && !imeFilma)
+                    {
+                        lvProjekcije.Items.Add(item);
+                    }
+                    else if (imeFilma && sala)
+                    {
+                        lvProjekcije.Items.Add(item);
+                    }
+                    //else if (sala && imeFilma)
+                    //{
+                    //    lvProjekcije.Items.Add(item);
+                    //}
+                }
+            }
+        }
+
+        private void rezervisi(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewClear()
+        {
+            lvProjekcije.Items.Clear();
+        }
+
+        private void racunajCenu(object sender, EventArgs e)
+        {
+            if (selectedItem != null)
+            {
+                cena = selectedItem.CenaKarte * (double)numBrojMesta.Value;
+                txtCena.Text = cena.ToString("0.00");
+                if (numBrojMesta.Value == 0)
+                {
+                    btnRezervisi.Enabled = false;
+                }
+                else
+                {
+                    btnRezervisi.Enabled = true;
+                }
+            }
+        }
+
+        private void resetFilter(object sender, EventArgs e)
+        {
+            listViewClear();
+            ucitajPodatke(sender, e);
         }
     }
 }
